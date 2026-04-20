a2881c3ef365cdfe7a0bcc2908f2c8f10315e4e8
Ayden Carroll <carrollan@vcu.edu>
2026-04-20 12:07:16 -0400

    Bug fix

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbarHTML = `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="font-family:'Montserrat'">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
         <a class="navbar-brand text-light p-0 fs-4" href="/index.html"><i class="bi bi-house"></i></a>
 
